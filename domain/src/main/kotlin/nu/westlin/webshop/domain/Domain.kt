aef0897cb97b8cd9fc33477f736b382a0b4f966a
Peter Vestlin <peter.vestlin@lm.se>
2020-10-22 07:02:33 +0200

    Refact

--- domain/src/main/kotlin/nu/westlin/webshop/domain/Domain.kt
+++ domain/src/main/kotlin/nu/westlin/webshop/domain/Domain.kt
@@ -5,8 +5,8 @@ import java.time.LocalDateTime
 typealias CustomerId = Int
 typealias ProductId = Int
 
-data class Customer(val id: Int, val name: String)
+data class Customer(val id: CustomerId, val name: String)
 
-data class Product(val id: Int, val name: String, val description: String? = null)
+data class Product(val id: ProductId, val name: String, val description: String? = null)
 data class OrderRow(val productId: ProductId, val quantity: Int)
 data class Order(val id: Int, val customerId: CustomerId, val timestamp: LocalDateTime, val orderRows: List<OrderRow>)
